d8a700afea299e23786a8bb3407509128114059d
sadramoh <shockylemi@gmail.com>
2025-06-02 21:17:47 +0200

    feat(eval): Two Plus Two

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -2,10 +2,10 @@ use std::collections::HashMap;
 
 use crate::ast::*;
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Default)]
 pub struct Variables(HashMap<String, Value>);
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub enum Value {
     Number(f64),
     Boolean(bool),
@@ -14,7 +14,7 @@ pub enum Value {
     Nil,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Default)]
 pub struct Functions(HashMap<String, FunctionDeclaration>);
 
 pub fn evaluate(xp: Expression, vars: &mut Variables, fns: &mut Functions) -> Option<Value> {
@@ -60,7 +60,79 @@ pub fn evaluate(xp: Expression, vars: &mut Variables, fns: &mut Functions) -> Op
         Expression::Member(member) => todo!(),
         Expression::Call(call) => todo!(),
         Expression::Identifier(identifier) => todo!(),
-        Expression::Literal(literal) => todo!(),
-        Expression::Dyadic(dyadic) => todo!(),
+        Expression::Literal(literal) => match literal {
+            Literal::Array => todo!(),
+            Literal::Tuple => todo!(),
+            Literal::Boolean(boolean_literal) => Some(Value::Boolean(boolean_literal.value)),
+            Literal::Character(character_literal) => {
+                Some(Value::Character(character_literal.value))
+            }
+            Literal::String(string_literal) => Some(Value::String(string_literal.value)),
+            Literal::Decimal(decimal_literal) => Some(Value::Number(decimal_literal.value)),
+            Literal::Hexadecimal(hexadecimal_literal) => {
+                Some(Value::Number(hexadecimal_literal.value as f64))
+            }
+            Literal::Binary(binary_literal) => Some(Value::Number(binary_literal.value as f64)),
+            Literal::Octal(octal_literal) => Some(Value::Number(octal_literal.value as f64)),
+        },
+        Expression::Dyadic(dyadic) => {
+            let left = evaluate(*dyadic.left, vars, fns);
+            let right = evaluate(*dyadic.right, vars, fns);
+
+            let Some(left) = left else { return None };
+            let Some(right) = right else { return None };
+
+            match dyadic.operator {
+                DyadicOperator::Add => match (left, right) {
+                    (Value::Number(left), Value::Number(right)) => {
+                        Some(Value::Number(left + right))
+                    }
+                    _ => None,
+                },
+                DyadicOperator::Subtract => match (left, right) {
+                    (Value::Number(left), Value::Number(right)) => {
+                        Some(Value::Number(left + right))
+                    }
+                    _ => None,
+                },
+                DyadicOperator::Multiply => todo!(),
+                DyadicOperator::Divide => todo!(),
+                DyadicOperator::Modulo => todo!(),
+                DyadicOperator::Power => todo!(),
+                DyadicOperator::Equal => todo!(),
+                DyadicOperator::NotEqual => todo!(),
+                DyadicOperator::LessThan => todo!(),
+                DyadicOperator::GreaterThan => todo!(),
+                DyadicOperator::LessThanOrEqual => todo!(),
+                DyadicOperator::GreaterThanOrEqual => todo!(),
+                DyadicOperator::And => todo!(),
+                DyadicOperator::Or => todo!(),
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        evaluator::{Functions, Value, Variables, evaluate},
+        parser::parse_program,
+    };
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    pub fn two_plus_two() {
+        let parsed = parse_program("2+2");
+        let evaluated = evaluate(
+            parsed
+                .body
+                .into_iter()
+                .next()
+                .expect("Expected at least one expression"),
+            &mut Variables::default(),
+            &mut Functions::default(),
+        );
+
+        assert_eq!(evaluated, Some(Value::Number(4.0)))
     }
 }
